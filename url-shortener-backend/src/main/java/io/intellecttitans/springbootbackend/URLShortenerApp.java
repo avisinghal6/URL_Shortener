bd6b30471dbda35ea5fd525942ea60875cb3703c
avisinghal6 <avisinghal6@gmail.com>
2023-11-03 00:01:05 -0500

    added starter code of backend

---
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/URLShortenerApp.java
@@ -0,0 +1,14 @@
+package io.intellecttitans.springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class URLShortenerApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(URLShortenerApp.class, args);
+	}
+
+}
