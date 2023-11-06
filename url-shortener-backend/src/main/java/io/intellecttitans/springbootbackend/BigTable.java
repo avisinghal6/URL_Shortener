7b58a20b85ec885db9e76f3e53b05082ffee8838
avisinghal6 <avisinghal6@gmail.com>
2023-11-05 23:31:00 -0600

    create BigTable object for all operations

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/BigTable.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/BigTable.java
@@ -6,18 +6,21 @@ import com.google.cloud.bigtable.data.v2.BigtableDataClient;
 import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
 import com.google.cloud.bigtable.data.v2.models.Row;
 import com.google.cloud.bigtable.data.v2.models.RowCell;
+import com.google.cloud.bigtable.data.v2.models.RowMutation;
+import com.google.protobuf.ByteString;
 
-public class ReadRow {
-
+public class BigTable {
+	String columnFamilyName;
 	BigtableDataClient dataClient;
 	String projectId;
 	String instanceId;
 	String tableId;
 
-	public ReadRow(String projectId, String instanceId, String tableId) {
+	public BigTable(String projectId, String instanceId, String tableId, String columnFamilyName) {
 		// Initialize client that will be used to send requests. This client only needs
 		// to be created once, and can be reused for multiple requests.
 		try {
+			this.columnFamilyName = columnFamilyName;
 			this.projectId = projectId;
 			this.instanceId = instanceId;
 			this.tableId = tableId;
@@ -33,10 +36,11 @@ public class ReadRow {
 	}
 
 	public static void main(String... args) {
-		ReadRow rowObj = new ReadRow("rice-comp-539-spring-2022", "rice-shared", "team2_urlshortener_urls");
-		
-		//Dummy testing code
-		rowObj.getRow("r1");
+		BigTable bigTable = new BigTable("rice-comp-539-spring-2022", "rice-shared", "team2_urlshortener_urls", "cf1");
+
+		// Dummy testing code
+		bigTable.writeRow("new data", "c1", "r1");
+		bigTable.getRow("r1");
 	}
 
 	public void getRow(String rowKey) {
@@ -53,6 +57,21 @@ public class ReadRow {
 			System.err.println("Failed to read from a non-existent table: " + e.getMessage());
 		} catch (Exception e) {
 			System.out.println("Error during quickstart: \n" + e.toString());
+		}
+	}
+
+	public void writeRow(String value, String subfamily, String rowKey) {
+		try {
+			long timestamp = System.currentTimeMillis() * 1000;
+
+			RowMutation rowMutation = RowMutation.create(tableId, rowKey)
+					.setCell(columnFamilyName, subfamily, value);
+
+			dataClient.mutateRow(rowMutation);
+			System.out.printf("Successfully wrote row %s", rowKey);
+
+		} catch (Exception e) {
+			System.out.println("Error during WriteSimple: \n" + e.toString());
 		}
 	}
 
