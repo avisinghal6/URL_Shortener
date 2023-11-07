08fde20e0df00da92f403a4ad1e5ad41e24fc5a0
avisinghal6 <avisinghal6@gmail.com>
2023-11-06 23:18:37 -0600

    added base62 encoding

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/Base64Encoding.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/Base64Encoding.java
@@ -1,15 +1,58 @@
 package io.intellecttitans.springbootbackend;
 import java.util.UUID;
+import java.util.Random;
 
 public class Base64Encoding {
 	
 	public static String base64Encoding() {
-		
+		String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        int base = characters.length();
+        
 		UUID uuid = UUID.randomUUID();
-        System.out.println("Random UUID: " + uuid);
-        return "";
-		
+        
+        long MSB = uuid.getMostSignificantBits();
+        long LSB = uuid.getLeastSignificantBits();
+
+        String output= base62Encode(MSB) + base62Encode(LSB);
+        System.out.println(output);
+        return output;
+        
 	}
+	
+	public static String base62Encode(long value) {
+        String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        int base = characters.length();
+        StringBuilder sb = new StringBuilder();
+        
+        value = Math.abs(value);
+        
+        
+        do {
+            sb.insert(0, characters.charAt((int)(value % base)));
+            value /= base;
+        } while (value > 0);
+        
+        return selectRandomCharacters(sb, 4);
+    }
+	
+	public static String selectRandomCharacters(StringBuilder inputString, int n) {
+        if (n <= 0) {
+            return ""; 
+        }
+
+        Random random = new Random();
+        int stringLength = inputString.length();
+        StringBuilder selectedChars = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+        	//Using uniform distribution
+            int randomIndex = random.nextInt(stringLength);
+            char selectedChar = inputString.charAt(randomIndex);
+            selectedChars.append(selectedChar);
+        }
+
+        return selectedChars.toString();
+    }
 	
 	public static void main(String... args) {
 		Base64Encoding.base64Encoding();
