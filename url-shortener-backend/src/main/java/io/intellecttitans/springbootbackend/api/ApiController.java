b629bc310fc19b19d897129a8931654e428188a4
avisinghal6 <avisinghal6@gmail.com>
2023-11-06 23:54:00 -0600

    added TODO for APIs

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/api/ApiController.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/api/ApiController.java
@@ -8,9 +8,23 @@ import io.intellecttitans.springbootbackend.Base62Encoding;
 public class ApiController {
 	@RequestMapping("/api/{longUrl}")
 	public String longToShortUrl(@PathVariable String longUrl ) {
-		
+		//TODO: insert the long,short URL in the big table.
 		String shortUrl=Base62Encoding.base62Encoding();
 		return shortUrl;
+	}
+	
+	@RequestMapping("/api/{shortUrl}")
+	public String shortToLongUrl(@PathVariable String shortUrl ) {
+		//TODO: query the data base to retrieve the long url for the corresponding short url.
+		
+		return "";
+	}
+	
+	@RequestMapping("/api/barcode/{longUrl}")
+	public String shortToLongUrlBarCode(@PathVariable String longUrl ) {
+		//TODO: create the short URL and bar code, return the bar code, save in database. The return type will be changed.
+		
+		return "";
 	}
 	
 }
