29f57378f006cf8d1614b565097d8c3b03ed4b61
avisinghal6 <avisinghal6@gmail.com>
2023-11-06 23:50:01 -0600

    completed longtoShortUrl, return encoded url

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/api/ApiController.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/api/ApiController.java
@@ -2,12 +2,15 @@ package io.intellecttitans.springbootbackend.api;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.intellecttitans.springbootbackend.Base62Encoding;
 
 @RestController
 public class ApiController {
 	@RequestMapping("/api/{longUrl}")
 	public String longToShortUrl(@PathVariable String longUrl ) {
-		return longUrl;
+		
+		String shortUrl=Base62Encoding.base62Encoding();
+		return shortUrl;
 	}
 	
 }
