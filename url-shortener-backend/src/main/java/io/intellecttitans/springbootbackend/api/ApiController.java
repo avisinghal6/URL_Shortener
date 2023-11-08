2560a0f80da485f8c456233a3eccae0b777b77ae
avisinghal6 <avisinghal6@gmail.com>
2023-11-07 20:34:10 -0600

    added QR code functionality

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/api/ApiController.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/api/ApiController.java
@@ -1,8 +1,15 @@
 package io.intellecttitans.springbootbackend.api;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+import org.krysalis.barcode4j.impl.upcean.EAN13Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +17,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import io.intellecttitans.springbootbackend.BigTable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
 import io.intellecttitans.springbootbackend.Base62Encoding;
 
 @RestController
@@ -47,11 +60,29 @@ public class ApiController {
 	}
 
 	@RequestMapping("/api/barcode/{longUrl}")
-	public String shortToLongUrlBarCode(@PathVariable String longUrl) {
+	public String shortToLongUrlBarCode(@PathVariable String longUrl) throws Exception  {
 		// TODO: create the short URL and bar code, return the bar code, save in
 		// database. The return type will be changed.
+		
 
-		return "";
+//	    barcodeGenerator.generateBarcode(canvas, "avisinghal.com");
+	    
+	    
+	    try {
+	    	QRCodeWriter barcodeWriter = new QRCodeWriter();
+		    BitMatrix bitMatrix = 
+		      barcodeWriter.encode(longUrl, BarcodeFormat.QR_CODE, 200, 200);
+		    
+		    BufferedImage image= MatrixToImageWriter.toBufferedImage(bitMatrix);
+		    
+	        File outputFile = new File("barcode.png"); // Replace with the desired file path
+	        ImageIO.write(image, "PNG", outputFile);
+	        System.out.println("Barcode image saved to " + outputFile.getAbsolutePath());
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+		
+		return "QR generated successfully";
 	}
 
 }
