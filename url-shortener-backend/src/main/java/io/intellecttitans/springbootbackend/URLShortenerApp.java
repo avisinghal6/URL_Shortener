5e55631d545ac801604a771db182e313cb24dd65
avisinghal6 <avisinghal6@gmail.com>
2023-11-25 16:41:57 -0600

    added google oauth

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/URLShortenerApp.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/URLShortenerApp.java
@@ -2,13 +2,26 @@ package io.intellecttitans.springbootbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class URLShortenerApp {
+public class URLShortenerApp extends WebSecurityConfigurerAdapter{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(URLShortenerApp.class, args);
 	}
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	// @formatter:off
+        http
+            .authorizeRequests()
+            .antMatchers("/hello")
+            .permitAll().anyRequest().authenticated()
+            .and().oauth2Login();
+        // @formatter:on
+    }
 
 }
