0fe70e2205e176f0e00201415aa08dbf73e0a437
avisinghal6 <avisinghal6@gmail.com>
2023-11-26 00:40:43 -0600

    added custom oauth

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/URLShortenerApp.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/URLShortenerApp.java
@@ -1,9 +1,18 @@
 package io.intellecttitans.springbootbackend;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @SpringBootApplication
 public class URLShortenerApp extends WebSecurityConfigurerAdapter{
@@ -20,8 +29,25 @@ public class URLShortenerApp extends WebSecurityConfigurerAdapter{
             .authorizeRequests()
             .antMatchers("/hello")
             .permitAll().anyRequest().authenticated()
-            .and().oauth2Login();
+            .and().oauth2Login()
+            .userInfoEndpoint()
+            .userService(oauthUserService).and()
+            .successHandler(new AuthenticationSuccessHandler() {
+                @Override
+                public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                        Authentication authentication) throws IOException, ServletException {
+         
+                    CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
+         
+                    System.out.println(oauthUser.getEmail());
+         
+                    response.sendRedirect("/hello");
+                }
+            });
         // @formatter:on
     }
+	
+	@Autowired
+    private CustomOAuth2UserService oauthUserService;
 
 }
