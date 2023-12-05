12bdd10b2ff9ccfa9c9e3af7f81d61432cdce8e5
Ishita Kundaliya <kundaliyaishita@gmail.com>
2023-12-05 00:23:07 -0600

    added post
    fixes #1
    fixes #3

--- url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/controllers/ApiController.java
+++ url-shortener-backend/src/main/java/io/intellecttitans/springbootbackend/controllers/ApiController.java
@@ -2,6 +2,7 @@ package io.intellecttitans.springbootbackend.controllers;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Date;
 import javax.imageio.ImageIO;
@@ -34,6 +35,13 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ApiController {
@@ -44,8 +52,8 @@ public class ApiController {
 	@Autowired
 	private UserTable userTable;
 
-	@RequestMapping(value="/api/longurl/{longUrl}",method = RequestMethod.POST,consumes = "application/x-www-form-urlencoded")
-	public ResponseEntity<String> longToShortUrl(@RequestParam("longurl") String long_url) {
+	@RequestMapping(value="/api/longurl",method = RequestMethod.POST,consumes = "application/x-www-form-urlencoded")
+	public ResponseEntity<String>longToShortUrl(@RequestParam("longurl") String long_url) {
 		Date currentDate = new Date();
 		String shortUrl = Base62Encoding.base62Encoding();
 		List<String> subFamily = new ArrayList<>();
@@ -75,8 +83,8 @@ public class ApiController {
 			}
 		}
 		
-		System.out.println(long_url);
-		return new ResponseEntity<>(shortUrl, HttpStatus.OK);
+		return new ResponseEntity<>("qwklnk.com/"+shortUrl, HttpStatus.OK);
+		
 	}
 //	
 //	@RequestMapping("/api/longurl/{long_url}")
@@ -101,40 +109,82 @@ public class ApiController {
 //		System.out.println(long_url);
 //		return new ResponseEntity<>(shortUrl, HttpStatus.OK);
 //	}
+	
+	@RequestMapping(value="/api/shorturl",method = RequestMethod.POST,consumes = "application/x-www-form-urlencoded")
+	public ResponseEntity<String> shortToLongUrl(@RequestParam("shorturl") String shortUrl){
 
-	@RequestMapping("/api/shorturl/{shortUrl}")
-	public ResponseEntity<String> shortToLongUrl(@PathVariable String shortUrl) {
-		List<String> longUrl = urlTable.getRow(shortUrl);
-		if(longUrl!=null)
-			return new ResponseEntity<>(longUrl.get(1), HttpStatus.OK);
+		List<String> longUrl = urlTable.getRow(shortUrl.substring(13, 21));
+		if(longUrl!=null) {			
+			return new ResponseEntity<>(longUrl.get(1).substring(12, longUrl.get(1).length() - 2), HttpStatus.OK);
+		}
 		else
 			return new ResponseEntity<>("Error getting the longUrl", HttpStatus.FORBIDDEN);
-	}
-
-	@RequestMapping("/api/barcode/{longUrl}")
-	public String shortToLongUrlBarCode(@PathVariable String longUrl) throws Exception  {
-		// TODO: create the short URL and bar code, return the bar code, save in
-		// database. The return type will be changed.
 		
-
-//	    barcodeGenerator.generateBarcode(canvas, "avisinghal.com");
-	    
-	    
-	    try {
+	}
+	
+	
+//	@RequestMapping("/api/shorturl/{shortUrl}")
+//	public ResponseEntity<String> shortToLongUrl(@PathVariable String shortUrl) {
+//		List<String> longUrl = urlTable.getRow(shortUrl);
+//		if(longUrl!=null)
+//			return new ResponseEntity<>(longUrl.get(1), HttpStatus.OK);
+//		else
+//			return new ResponseEntity<>("Error getting the longUrl", HttpStatus.FORBIDDEN);
+//	}
+	
+	@RequestMapping(value="/api/barcode",method = RequestMethod.POST,consumes = "application/x-www-form-urlencoded")
+	public ResponseEntity<String> shortToLongUrlBarCode(@RequestParam("longurl") String longUrl) throws Exception{
+		System.out.println(longUrl);
+		try {
 	    	QRCodeWriter barcodeWriter = new QRCodeWriter();
 		    BitMatrix bitMatrix = 
-		      barcodeWriter.encode(longUrl, BarcodeFormat.QR_CODE, 200, 200);
+		      barcodeWriter.encode(longUrl.substring(12, longUrl.length() - 2), BarcodeFormat.QR_CODE, 200, 200);
 		    
 		    BufferedImage image= MatrixToImageWriter.toBufferedImage(bitMatrix);
-		    
-	        File outputFile = new File("barcode.png"); // Replace with the desired file path
-	        ImageIO.write(image, "PNG", outputFile);
-	        System.out.println("Barcode image saved to " + outputFile.getAbsolutePath());
+
+		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		    ImageIO.write(image, "PNG", baos);
+		    byte[] bytes = baos.toByteArray();
+		    String bytesBase64 = Base64.encodeBase64String(bytes);
+
+	        return new ResponseEntity<>(bytesBase64, HttpStatus.OK);
 	    } catch (IOException e) {
 	        e.printStackTrace();
+	        return new ResponseEntity<>("Error generating barcode", HttpStatus.FORBIDDEN);   
 	    }
-		
-		return "QR generated successfully";
 	}
+//	@RequestMapping("/api/barcode/{longUrl}")
+//	public String shortToLongUrlBarCode(@PathVariable String longUrl) throws Exception  {
+//		System.out.println("in QR code");
+//		// TODO: create the short URL and bar code, return the bar code, save in
+//		// database. The return type will be changed.
+//		
+//
+////	    barcodeGenerator.generateBarcode(canvas, "avisinghal.com");
+//	    
+//	    
+//	    try {
+//	    	QRCodeWriter barcodeWriter = new QRCodeWriter();
+//		    BitMatrix bitMatrix = 
+//		      barcodeWriter.encode(longUrl, BarcodeFormat.QR_CODE, 200, 200);
+//		    
+//		    BufferedImage image= MatrixToImageWriter.toBufferedImage(bitMatrix);
+//		    System.out.println("..................");
+//		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//		    ImageIO.write(image, "PNG", baos);
+//		    byte[] bytes = baos.toByteArray();
+//		    String bytesBase64 = Base64.encodeBase64String(bytes);
+//		    System.out.println(bytesBase64);
+//		    
+//		    System.out.println("..................");
+//	        File outputFile = new File("barcode.png"); // Replace with the desired file path
+//	        ImageIO.write(image, "PNG", outputFile);
+//	        System.out.println("Barcode image saved to " + outputFile.getAbsolutePath());
+//	    } catch (IOException e) {
+//	        e.printStackTrace();
+//	    }
+//		
+//		return "yaas queen";
+//	}
 
 }
